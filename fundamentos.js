641fd31fc453f8d286d5a883c3eca464e7cfbed4
Estevão <estevaosilva.sg@gmail.com>
2023-07-31 17:06:42 -0300

    Fundamentos 2

--- fundamentos.js
+++ fundamentos.js
@@ -15,6 +15,7 @@ console.log(minhavariavel);
  várias
  linhas */
 
+ // 2.3 Variáveis e tipos de dados
  var meuNumero = 7;
  console.log(meuNumero);
  console.log(typeof meuNumero);
@@ -41,6 +42,7 @@ console.log(minhavariavel);
  console.log(typeof minhaNull);
  console.log(typeof minhaUndefined);
 
+// 2.4 let e const
 
  let teste = 1; // Pode mudar ao longo do processo
  const PI = 3.14; // Não pode mudar ao longo do processo MAIS SEGURA
@@ -50,19 +52,65 @@ console.log(minhavariavel);
  const x = 7;
  const y = 9;
 
+ // Operadores aritméticos
  console.log(x + y);
  console.log(x - y);
  console.log(x * y);
  console.log(x / y);
  
+ // Operadores de comparação
 console.log(x == y);
 console.log(x === y);
 console.log(x !== y);
 console.log(x != y);
 
-
+// Operadores lógicos
 console.log( 1 > 0 && 0 > 1);
 console.log( 1 > 0 && 0 < 1);
 
 console.log( 1 > 0 || 0 > 1);
 console.log( 1 > 0 || 0 < 1);
+
+// 2.6 Conversão de tipos
+const meuNumero2 = "123;"; //String
+console.log(typeof meuNumero2);
+const meuNumeroConvertido = Number(meuNumero2);
+console.log(typeof meuNumeroConvertido);
+
+
+// 2.7 Estruturas de controle de fluxo
+
+// Condição if, else if, else
+const idade = 18;
+
+if (idade < 13) {
+    console.log("Criança");
+} else if (idade < 18 && idade > 13) { 
+    console.log("Adolescente");
+} else {
+    console.log("Adulto");
+}
+
+// Condição switch
+const fruta = "Maçã";
+
+switch (fruta) {
+    case "Maçã":
+        console.log("Tem maçã!");   
+        break    
+    case "Banana":
+        console.log("Tem banana!");
+        break
+    case "Goiaba":
+        console.log("Tem goiaba!")
+        break
+    default:
+        console.log("Não tem essa fruta!")
+}
+
+// 2.7 Estruturas de loop
+// Loop fo
+
+for (let i = 0; i < 5; i + 1) {
+    console.log(i);
+}
